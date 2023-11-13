590d6509a813b45f48b79f811ce90dcf38420a56
fufel_mpr0qo5 <fufel_mpr0qo5@BROOMBO>
2023-11-14 02:43:30 +0300

    с окнами

---
+++ CinemaProject/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CinemaProject.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Enter(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+
+        }
+
+        private void Button_Reg(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CinemaProject/Windows/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CinemaProject.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Employee(object sender, RoutedEventArgs e)
+        {
+            EmployeesWindow employeesWindow = new EmployeesWindow();
+            employeesWindow.Show();
+            Close();
+        }
+        private void Button_Click_Client(object sender, RoutedEventArgs e)
+        {
+            ClientsWindow clientsWindow = new ClientsWindow();
+            clientsWindow.Show();
+            Close();
+        }
+        private void Button_Click_Session(object sender, RoutedEventArgs e)
+        {
+            SessionsWindow sessionsWindow = new SessionsWindow();  
+            sessionsWindow.Show();
+            Close();
+        }
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+        }
+
+    }
+}
